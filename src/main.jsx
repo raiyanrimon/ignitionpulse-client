1114a491f54fd7f3a2a932cc5978b4817b1bd0d4
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-11-27 00:32:20 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,26 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Layout/Root";
 import ErrorPage from "./ErrorPage/ErrorPage";
+import Home from "./Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
 );
